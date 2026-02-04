8a2a311cc98c43d0a2c742e9bc8a2b4f783268f1
omarakamal <omaraakamal@gmail.com>
2026-02-04 19:05:46 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,19 @@ function App() {
     setCount(count + 1)
   }
 
+  function handleDecrease(){
+    if(count ===0){
+      return
+    }
+    setCount(count - 1)
+  }
+
+  function handleReset(){
+    setCount(0)
+  }
+
+
+
   // Exercise 1:
   // 1. Create a - button
   // 2. Create a function handleDecrease() that should decrease the count by 1 (remember use the updater function)
@@ -17,6 +30,8 @@ function App() {
     <>
     <p>{count}</p>
     <button onClick={handleIncrease}>+</button>
+    <button onClick={handleDecrease}>-</button>
+    <button onClick={handleReset}>reset</button>
     </>
   )
 }
