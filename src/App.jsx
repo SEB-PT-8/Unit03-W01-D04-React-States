5b871a64c4d9862e3e3fe3f647fac1cd7a6bdd5f
omarakamal <omaraakamal@gmail.com>
2026-02-04 19:52:00 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import { useState } from "react"
 function App() {
   
+  const allStudents = [
+        {
+          id:1,
+          studentName: "Ali",
+          grade: 101,
+          city:"Manama"
+        },
+        {
+          id:2,
+          studentName: "Mohammad",
+          grade: 99,
+          city:"Manama2"
+        },
+        {
+          id:3,
+          studentName: "Muqtada",
+          grade: 100,
+          city:"Manama3"
+        },
+      ]
   //    [state, updater function] = useState(initial value)
   const [count, setCount] = useState(0)
+  const [students,setStudents] = useState(allStudents)
+
 
   function handleIncrease(){
     setCount(count + 1)
@@ -26,12 +48,23 @@ function App() {
   // 2. Create a function handleDecrease() that should decrease the count by 1 (remember use the updater function)
   // 3. BONUS: count cant go below 0
   // 4. BONUS BONUS: add a reset button that should reset the count to 0
+
+  console.log("Component loads")
   return (
     <>
     <p>{count}</p>
     <button onClick={handleIncrease}>+</button>
     <button onClick={handleDecrease}>-</button>
     <button onClick={handleReset}>reset</button>
+
+    {students.map((oneStudent)=>
+    <div>
+      <p>Name: {oneStudent.studentName}</p>
+      <p>grade: {oneStudent.grade}</p>
+      <p>city: {oneStudent.city}</p>
+      <button>Remove student</button>
+    </div>
+    )}
     </>
   )
 }
