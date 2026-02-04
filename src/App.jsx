c03eb01f140c12127f22dec1ebebe01ad6d89760
omarakamal <omaraakamal@gmail.com>
2026-02-04 18:53:39 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ function App() {
   //    [state, updater function] = useState(initial value)
   const [count, setCount] = useState(0)
 
+  function handleIncrease(){
+    setCount(count + 1)
+  }
+
+  // Exercise 1:
+  // 1. Create a - button
+  // 2. Create a function handleDecrease() that should decrease the count by 1 (remember use the updater function)
+  // 3. BONUS: count cant go below 0
+  // 4. BONUS BONUS: add a reset button that should reset the count to 0
   return (
     <>
-    
+    <p>{count}</p>
+    <button onClick={handleIncrease}>+</button>
     </>
   )
 }
