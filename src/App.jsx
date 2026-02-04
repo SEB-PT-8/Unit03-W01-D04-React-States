4be8483de19fb9df778fa929fcd79d986148121a
omarakamal <omaraakamal@gmail.com>
2026-02-04 18:32:37 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+function App() {
+  
+  //    [state, updater function] = useState(initial value)
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
